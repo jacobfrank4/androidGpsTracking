228308d1c2cc05cba392134ee428272cca70711c
Jacob Frank <jacobfrank4@gmail.com>
2016-11-02 22:42:47 -0700

    Created method for sending the users GPS location via SMS

--- app/src/main/java/bcit/androidgpstracking/MainActivity.java
+++ app/src/main/java/bcit/androidgpstracking/MainActivity.java
@@ -5,6 +5,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.TextView;
 
@@ -26,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
 	public void getGPS(final View view) {
 		LocationListener locationListener = new MyLocationListener(output);
 		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+	}
+
+	/*
+	Method for sending a text to a list of recipients that contains the GPS location.
+	Future improvements are:
+	1) convert method of handling databases into a class for handling texting
+	2) pull recipient list from Trip list.
+	 */
+	public void sendGPSText(final View view) {
+		SmsManager smsManager = SmsManager.getDefault();
+		String numbers[] = {};
+		String message = output.getText().toString();
+		for(int i = 0; i < numbers.length; i++) {
+			smsManager.sendTextMessage(numbers[i], null, message, null, null);
+		}
 	}
 }
