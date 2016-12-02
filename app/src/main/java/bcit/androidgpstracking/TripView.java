94664006fb2758b22fd66b6729c0992d5bc9abe4
Jacob Frank <jacobfrank4@gmail.com>
2016-12-01 22:10:16 -0800

    made it beautiful

--- app/src/main/java/bcit/androidgpstracking/TripView.java
+++ app/src/main/java/bcit/androidgpstracking/TripView.java
@@ -1,7 +1,15 @@
 package bcit.androidgpstracking;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +18,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
 public class TripView extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    SQLiteDatabaseHelper db;
+    String trip;
+    TableLayout tl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +38,50 @@ public class TripView extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        db = new SQLiteDatabaseHelper(this);
+        tl = (TableLayout) findViewById(R.id.gpsDataTable);
+
+        Intent intent = getIntent();
+        trip = intent.getStringExtra("tripName");
+
+        addData();
     }
 
+
+    public void addData(){
+        Cursor cursor = db.getTripData(trip);
+        if(cursor.getCount() == 0){
+            Log.d(TAG, "No data found in db");
+        }else{
+            StringBuffer latlongbuff = new StringBuffer();
+            StringBuffer timebuff = new StringBuffer();
+
+            while(cursor.moveToNext()){
+
+                TableRow row = new TableRow(this);
+                TextView latlong = new TextView(this);
+                TextView time = new TextView(this);
+
+                TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+                row.setLayoutParams(lp);
+
+                timebuff.append(cursor.getString(4)); //date/time
+                latlongbuff.append(cursor.getString(2)); //latitude
+                latlongbuff.append("; "); //latitude
+                latlongbuff.append(cursor.getString(3)); //longitude
+
+                latlong.setText(latlongbuff);
+                time.setText(timebuff);
+
+                row.addView(latlong);
+                row.addView(time);
+
+                tl.addView(row);
+                latlongbuff.delete(0, latlongbuff.length());
+                timebuff.delete(0, timebuff.length());
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -37,10 +95,75 @@ public class TripView extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        Cursor cursor = db.getTripData(trip);
+        List<Double> latArray;
+        List<Double> longArray;
+        latArray = new ArrayList<Double>();
+        longArray = new ArrayList<Double>();
+        double longitudeAverage = 0;
+        double latitudeAverage = 0;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if(cursor.getCount() == 0){
+            Log.d(TAG, "No data found in db");
+        }else{
+            StringBuffer namebuff = new StringBuffer();
+            StringBuffer latbuff = new StringBuffer();
+            StringBuffer longbuff = new StringBuffer();
+            String lat;
+            String lon;
+            double latitude;
+            double longitude;
+
+            while(cursor.moveToNext()){
+
+                namebuff.append(cursor.getString(10)); //trip name
+                latbuff.append(cursor.getString(2)); //latitude
+                longbuff.append(cursor.getString(3)); //longitude
+
+                lat =  latbuff.toString();
+                lon =  longbuff.toString();
+
+                if(lat.charAt(lat.length()-1) == 'S') {
+                    lat = lat.substring(0, lat.length()-1);
+                    latitude = -Double.parseDouble(lat);
+
+                } else {
+                    lat = lat.substring(0, lat.length()-1);
+                    latitude = Double.parseDouble(lat);
+                }
+
+                if(lon.charAt(lon.length()-1) == 'W') {
+                    lon = lon.substring(0, lon.length()-1);
+                    longitude = -Double.parseDouble(lon);
+
+                } else {
+                    lon = lon.substring(0, lon.length()-1);
+                    longitude = Double.parseDouble(lon);
+                }
+
+                latArray.add(latitude);
+                longArray.add(longitude);
+
+
+                // Add a trip marker
+                LatLng tripMarker = new LatLng(latitude,longitude);
+                mMap.addMarker(new MarkerOptions().position(tripMarker).title(namebuff.toString()));
+
+                namebuff.delete(0, namebuff.length());
+                latbuff.delete(0, latbuff.length());
+                longbuff.delete(0, longbuff.length());
+            }
+            for(int i = 0; i < latArray.size(); i++) {
+                latitudeAverage += latArray.get(i);
+            }
+            latitudeAverage = (latitudeAverage/latArray.size());
+            for(int i = 0; i < longArray.size(); i++) {
+                longitudeAverage += longArray.get(i);
+            }
+            longitudeAverage = (longitudeAverage/longArray.size());
+
+
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitudeAverage, longitudeAverage), 8.0f));
+        }
     }
 }
